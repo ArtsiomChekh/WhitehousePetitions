4111e924333825d4b18913e6aeabe9d124bb03d7
Artsiom Chekh <qbartem@gmail.com>
2024-02-29 14:32:46 +0300

    Added filter logic.

--- WhitehousePetitions/ViewController.swift
+++ WhitehousePetitions/ViewController.swift
@@ -9,12 +9,17 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString: String
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .rewind, target: self, action: #selector(showApi))
         
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(promptForSearch))
+        
+        let urlString: String
+            
         if navigationController?.tabBarItem.tag == 0 {
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
         } else {
@@ -28,6 +33,36 @@ class ViewController: UITableViewController {
             }
         }
         showError()
+    }
+    
+    @objc func showApi() {
+        let ac = UIAlertController(title: "The data comes from the We The People API of the Whitehouse.", message: "https://api.whitehouse.gov/v1/petitions.json?limit=100", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "ОК", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func promptForSearch() {
+        let ac = UIAlertController(title: "Search", message: "Enter your keyword", preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] _ in
+            guard let word = ac?.textFields?[0].text else { return }
+            self?.filter(word: word)
+        }
+                
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func filter(word: String) {
+        filteredPetitions.removeAll()
+        for potition in petitions {
+            if potition.title.contains(word) || potition.body.contains(word) {
+                filteredPetitions.append(potition)
+            }
+        }
+        tableView.reloadData()
     }
 
     func showError() {
@@ -43,15 +78,16 @@ class ViewController: UITableViewController {
             petitions = jsonPetitions.results
             tableView.reloadData()
         }
+        filteredPetitions = petitions
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        petitions.count
+        filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
@@ -59,7 +95,7 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailVC()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
 }
